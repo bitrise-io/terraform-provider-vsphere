800d2cb10b4983792d0cb967e4c848862b4d24ae
virago <otto.virag@birtise.io>
2019-05-15 09:51:25 +0200

    add debug log to keepalive function

--- vendor/github.com/vmware/govmomi/session/keep_alive.go
+++ vendor/github.com/vmware/govmomi/session/keep_alive.go
@@ -41,9 +41,8 @@ type keepAlive struct {
 }
 
 func defaultKeepAlive(roundTripper soap.RoundTripper) error {
+	log.Printf("[DEBUG] CALL KEEPALIVE: TIME: %s, ERROR: %s", time.Format("2006-01-02 15:04:05"), err)
 	time, err := methods.GetCurrentTime(context.Background(), roundTripper)
-
-	log.Printf("CALL KEEPALIVE: TIME: %s, ERROR: %s", time.Format("2006-01-02 15:04:05"), err)
 
 	return nil
 }
